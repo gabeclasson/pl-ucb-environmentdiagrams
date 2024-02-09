08b82ebcb462d5f2aa3fbbe66b1939f8bad8466f
Gabriel Classon <gabriel.classon@gmail.com>
2024-02-09 13:37:09 -0800

    add frame

--- lowfi-ui/main.js
+++ lowfi-ui/main.js
@@ -1,32 +1,35 @@
-function add_variable(variables) {
+function add_variable(e) {
+  console.log(e)
+  button = e.target
+  frame = button.closest(".stackFrame")
+  variables = frame.querySelector(".stackFrameVarTable")
 	//var newvar = document.createElement("div");
   const tr = variables.insertRow();
+  tr.classList.add("variableTr")
   var removevar = document.createElement("button");
   var varname = document.createElement("input");
-  var stackframeval = document.createElement("div");
-  var test = document.createElement("input");
+  var stackframeval = document.createElement("input");
   
   removevar.innerHTML = "x";
   removevar.onclick = function(){variables.deleteRow(tr)};
-  
-  test.className = "stringObj";
-  test.size = 4;
-  test.innerHTML = "hello";
-  stackframeval.className = "stackFrameValue";
+
   
   /*newvar.appendChild(removevar);
   newvar.appendChild(varname);
   newvar.appendChild(stackframeval);*/
   var td = tr.insertCell();
+  td.classList.add()
   td.appendChild(removevar);
   var td = tr.insertCell();
+  td.classList.add("stackFrameVar")
   td.appendChild(varname);
   var td = tr.insertCell();
-  stackframeval.appendChild(test);
+  td.classList.add("stackFrameValue")
 	td.appendChild(stackframeval);
 	//variables.appendChild();
 }
-function add_frame(cell, name) {
+
+function add_frame() {
 	var frame = document.createElement("div");
   var removeframe = document.createElement("button");
   var frame_name_label = document.createElement("label");
@@ -48,7 +51,7 @@ function add_frame(cell, name) {
   removeframe.innerHTML = "x";
   removeframe.onclick = function() {
     // Remove the frame when the button is clicked
-    cell.removeChild(frame);
+    frame.removeChild(frame);
 };  
   variable_button.innerHTML = "add variable";
   variable_button.onclick = function(){
@@ -62,44 +65,10 @@ function add_frame(cell, name) {
   frame.appendChild(frame_name);
   frame.appendChild(variables);
   frame.appendChild(variable_button);
-  cell.appendChild(frame);
+  document.getElementById("globals_area").appendChild(frame);
   //document.body.appendChild(frame);
 }
 
-function load_environment_diagram() {
-	var stackHeapTable = document.createElement("table");
-  var object_buttons = document.createElement("div");
-  var function_button = document.createElement("button");
-  var list_button = document.createElement("button");
-  var frame_button = document.createElement("button");
-  var Frames = document.createElement("div");
-  var Objects = document.createElement("div");
-  
-  
-  Frames.className = "stackHeader";
-  Frames.innerHTML = "Frames";
-  frame_button.innerHTML = "add frame";
-  frame_button.onclick = function(){add_frame(td1, "");};
-  
-  Objects.className = "stackHeader";
-  Objects.innerHTML = "Objects";
-  function_button.innerHTML = "add function";
-  list_button.innerHTML = "add list";
-  
-  /*var tr = stackHeapTable.insertRow();
-  var td = tr.insertCell();
-  td.appendChild(Frames);
-  var td = tr.insertCell();
-  td.appendChild(Objects);*/
-  
-  var tr = stackHeapTable.insertRow();
-  var td1 = tr.insertCell();
-  td1.appendChild(frame_button);
-  td1.appendChild(Frames);
-  add_frame(td1, "global");
-  var td2 = tr.insertCell();
-  td2.appendChild(Objects);
-  td2.appendChild(function_button);
-  td2.appendChild(list_button);
-  document.body.appendChild(stackHeapTable);
-}
+let globalFrame = document.getElementById("globalFrame")
+globalFrame.querySelector(".addVarButton").addEventListener("click", add_variable)
+document.querySelector("#addFrameButton").addEventListener("click", add_frame)
