845eb6d59f50f9be36039582c8bb63a40bfd38ee
Gabriel Classon <gabriel.classon@gmail.com>
2024-02-09 14:42:50 -0800

    add function

--- lowfi-ui/main.js
+++ lowfi-ui/main.js
@@ -1,6 +1,5 @@
 var frameCount = 0;
 
-function add_variable(variables) {
 function add_variable(e) {
   console.log(e)
   button = e.target
@@ -9,17 +8,10 @@ function add_variable(e) {
 	//var newvar = document.createElement("div");
   const tr = variables.insertRow();
   tr.classList.add("variableTr")
-  var removevar = document.createElement("button");
+  var removevar = make_remove_button(tr);
   var varname = document.createElement("input");
   var stackframeval = document.createElement("input");
-  
-  removevar.innerHTML = "x";
-  removevar.onclick = function(){variables.deleteRow(tr)};
 
-  
-  /*newvar.appendChild(removevar);
-  newvar.appendChild(varname);
-  newvar.appendChild(stackframeval);*/
   var td = tr.insertCell();
   td.classList.add()
   td.appendChild(removevar);
@@ -29,7 +21,26 @@ function add_variable(e) {
   var td = tr.insertCell();
   td.classList.add("stackFrameValue")
 	td.appendChild(stackframeval);
-	//variables.appendChild();
+}
+
+function make_remove_button(target) {
+  var removeframe = document.createElement("button");
+  removeframe.className = "removeButton"
+  removeframe.onclick = function() {
+    target.parentElement.removeChild(target);
+  }
+  return removeframe
+}
+
+function make_parent_marker(elemType) {
+  var frame_parent_input = document.createElement("input");
+  frame_parent_input.className = "frameParentHeader";
+  marker = document.createElement(elemType)
+  marker.innerHTML = "[Parent = ";
+  marker.type = "text";
+  marker.appendChild(frame_parent_input);
+  marker.innerHTML += "]";
+  return marker
 }
 
 function add_frame() {
@@ -37,32 +48,23 @@ function add_frame() {
   var removeframe = document.createElement("button");
   var frame_name_label = document.createElement("label");
   var frame_parent_label = document.createElement("label");
-  var frame_parent_input = document.createElement("input");
   var frame_name = document.createElement("input");
   var variables = document.createElement("table");
   var variable_button = document.createElement("button");
-  var variable_count = 0;
   var test = document.createElement("div");
   
   frame.className = "stackFrame";
   frame_name_label.className = "frameHeader";
-  frame_parent_input.className = "frameParentHeader";
   frame_name.className = "frameHeader";
   
   test.className = "stackFrameValue";
   
-  variables.className = "frameTable";
-  removeframe.innerHTML = "x";
-  removeframe.onclick = function() {
-    cell.removeChild(frame);
-    // Remove the frame when the button is clicked
-    frame.removeChild(frame);
-};  
+  variables.className = "stackFrameVarTable";
+
+  removeframe = make_remove_button(frame);
+  
   variable_button.innerHTML = "add variable";
-  variable_button.onclick = function(){
-    add_variable(variables); 
-    variable_count = variable_count + 1;
-    };
+  variable_button.onclick = add_variable
 
   if (frameCount === 0) {
       frame_name_label.innerHTML = "Global frame";
@@ -71,10 +73,7 @@ function add_frame() {
       frame_name_label.inner
   }
 
-  frame_parent_label.innerHTML = "[Parent = ";
-  frame_parent_input.type = "text";
-  frame_parent_label.appendChild(frame_parent_input);
-  frame_parent_label.innerHTML += "]";
+  frame_parent_label = make_parent_marker("label")
   
   //frame.appendChild(test);
   frame.appendChild(removeframe);
@@ -83,44 +82,39 @@ function add_frame() {
   frame.appendChild(frame_parent_label);
   frame.appendChild(variables);
   frame.appendChild(variable_button);
-  cell.appendChild(frame);
 
   frameCount++;
   document.getElementById("globals_area").appendChild(frame);
   //document.body.appendChild(frame);
 }
 
-function load_environment_diagram() {
-	var stackHeapTable = document.createElement("table");
-  var object_buttons = document.createElement("div");
-  var function_button = document.createElement("button");
-  var list_button = document.createElement("button");
-  var frame_button = document.createElement("button");
-  var Frames = document.createElement("div");
-  var Objects = document.createElement("div");
-  
-  
-  Frames.className = "stackHeader";
-  Frames.innerHTML = "Frames";
-  frame_button.innerHTML = "add frame";
-  frame_button.onclick = function(){add_frame(td1, "");};
-  
-  Objects.className = "stackHeader";
-  Objects.innerHTML = "Objects";
-  function_button.innerHTML = "add function";
-  list_button.innerHTML = "add list";
-  
-  var tr = stackHeapTable.insertRow();
-  var td1 = tr.insertCell();
-  td1.appendChild(frame_button);
-  td1.appendChild(Frames);
-  add_frame(td1, "global");
-  var td2 = tr.insertCell();
-  td2.appendChild(Objects);
-  td2.appendChild(function_button);
-  td2.appendChild(list_button);
-  document.body.appendChild(stackHeapTable);
+let heapObjectCount = 0;
+function add_heap_object(content) {
+  let heapRow = document.createElement("table")
+  heapRow.className = "heapRow"
+  let topLevelHeapObject = document.createElement("td")
+  topLevelHeapObject.className = "topLevelHeapObject"
+  heapRow.appendChild(topLevelHeapObject)
+  heapRow.appendChild(make_remove_button(heapRow))
+  let heapObject = document.createElement("div")
+  heapObject.className = "heapObject";
+  topLevelHeapObject.appendChild(heapObject)
+  heapObject.appendChild(content)
+  document.getElementById("heap").appendChild(heapObject);
 }
+
+function add_function_object() {
+  let funcObj = document.createElement("div")
+  funcObj.className = "funcObj"
+  funcObj.innerText = "func "
+  let funcNameInput = document.createElement("input")
+  funcNameInput.className = "funcNameInput"
+  funcObj.appendChild(funcNameInput)
+  funcObj.appendChild(make_parent_marker("span"))
+  add_heap_object(funcObj)
+}
+
 let globalFrame = document.getElementById("globalFrame")
 globalFrame.querySelector(".addVarButton").addEventListener("click", add_variable)
 document.querySelector("#addFrameButton").addEventListener("click", add_frame)
+document.querySelector("#addFuncButton").addEventListener("click", add_function_object)
