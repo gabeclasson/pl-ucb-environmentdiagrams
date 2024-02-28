9fce49b62861b4f18522c8f2bd313ad3cb76c1ac
noemi <noemi.chulo@berkeley.edu>
2024-02-27 22:08:40 -0800

    moved Frame class from autoeval to frame.py.

--- elements/pl-env-diagram/autoeval.py
+++ elements/pl-env-diagram/autoeval.py
@@ -1,40 +1,8 @@
 
 import inspect
 import heapq
-
-class Frame():
-    # I, Noemi, will refer to this as a FrameNode for the time being to remove ambiguity with FrameObjects for the time being. 
-    is_global = False
-
-    def __init__(self, name = None, bindings=None, parent=None, fobj=None):
-        self.parent = parent
-        self.children = set()
-        if bindings is None:
-            self.bindings = {}
-        self.name = name
-        # self's frame object
-        self.fobj = fobj
-
-    def bind(self, name, value):
-        self.bindings[name] = value # Note: this will allow for duplicate entries. need to figure out a better way to do this. 
-    
-    def bind(self, name_value_dict):
-        self.bindings = name_value_dict # Note: this will allow for duplicate entries. need to figure out a better way to do this. 
-    
-    def set_name(self, name):
-        self.name = name
-    
-    def add_child(self, child):
-        self.children.add(child)
-    
-    def __str__(self, level=0):
-        ret = "\t"*level+repr(self.name)+"\n"
-        for child in self.children:
-            ret += child.__str__(level+1)
-        return ret
-
-    def __repr__(self):
-        return '<frame node representation>'
+import frame
+Frame = frame.Frame
 
 class Binding():
     def __init__(self, value):
@@ -178,7 +146,6 @@ class FrameTree():
         # TODO: REMOVE BELOW LINE(S) WHEN ALL CODE IS WORKING
         exitlines_pq= exitlines_pq.copy()
         code_list = self.codestring.split("\n")
-        print("exit:", exitlines_pq)
 
         # TODO: fix to include correct variable names
         # set variable names for things we are using to store tracking info. (TODO: CHNAGE NAME GLOBVAR)
--- elements/pl-env-diagram/frame.py
+++ elements/pl-env-diagram/frame.py
@@ -1,9 +1,33 @@
 class Frame():
+    # I, Noemi, will refer to this as a FrameNode for the time being to remove ambiguity with FrameObjects for the time being. 
     is_global = False
 
-    def __init__(self, parent=None, bindings=None):
-        if parent is None:
-            self.is_global = True
+    def __init__(self, name = None, bindings=None, parent=None, fobj=None):
         self.parent = parent
+        self.children = set()
         if bindings is None:
             self.bindings = {}
+        self.name = name
+        # self's frame object
+        self.fobj = fobj
+
+    def bind(self, name, value):
+        self.bindings[name] = value # Note: this will allow for duplicate entries. need to figure out a better way to do this. 
+    
+    def bind(self, name_value_dict):
+        self.bindings = name_value_dict # Note: this will allow for duplicate entries. need to figure out a better way to do this. 
+    
+    def set_name(self, name):
+        self.name = name
+    
+    def add_child(self, child):
+        self.children.add(child)
+    
+    def __str__(self, level=0):
+        ret = "\t"*level+repr(self.name)+"\n"
+        for child in self.children:
+            ret += child.__str__(level+1)
+        return ret
+
+    def __repr__(self):
+        return '<frame node representation>'
